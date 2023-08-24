73a7fc5f399c0695715498def88ba6b7ea91b13d
MiloHuy <91107200+MiloHuy@users.noreply.github.com>
2023-08-24 15:47:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Button} from "@nextui-org/react";
+
+function App() {
+  return (
+    <Button className="px-unit-2 py-unit-1 min-w-unit-3xl">
+      My Button
+    </Button>
+  );
+}
+
+export default App;
